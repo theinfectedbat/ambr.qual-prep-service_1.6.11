60b87dd7dad02d44d06f0cf5e5c9d41c0b2f5614
a8332 <a8332@MDDESK375.amberroad.com>
2019-01-11 15:22:53 +0530

    TA-82650
    BOM Mass Re-qualification from UI

---
+++ src/main/java/com/ambr/gtm/fta/qts/workmgmt/QtxStageData.java
@@ -0,0 +1,15 @@
+package com.ambr.gtm.fta.qts.workmgmt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QtxStageData
+{
+    public ArrayList<Long> altKeylist ;
+    
+    public ArrayList<String> agreementList;
+    
+    public boolean isforceQualification;
+    
+    public String reasonCode;
+}
