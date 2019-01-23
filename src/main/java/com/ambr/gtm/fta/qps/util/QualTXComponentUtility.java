ef36e6f2bf5053cab855a5b2755efd4b345ecf44
m2249 <m2249@MDLPTP245.amberroad.com>
2019-01-23 16:34:25 +0530

    Fix for SCR : TA-83051
    BOM_ConservativeSrc:BOM component Qualification should be set to NO,when
    there is NO STP identified in the Source which is pulled from the
    component.

--- src/main/java/com/ambr/gtm/fta/qps/util/QualTXComponentUtility.java
+++ src/main/java/com/ambr/gtm/fta/qps/util/QualTXComponentUtility.java
@@ -306,7 +306,7 @@ public class QualTXComponentUtility
 				this.qualTXComp.qualified_flg = (aConservativeSRCIVA.finalDecision == null)? "" : aQualified;
 				this.qualTXComp.prod_src_iva_key = aConservativeSRCIVA.ivaKey;
 			}
-			else {
+			else if(aConservativeSRCIVA == null || aConservativeSRCIVA.ivaKey == -1){
 				this.qualTXComp.qualified_flg =  "N" ;
 				this.qualTXComp.prod_src_iva_key = null;
 			}
