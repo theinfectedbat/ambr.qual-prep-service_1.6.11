7f4f1311fba4057b5f0beb7696cbb6d7234cab03
a8334 <a8334@MDDESK372.amberroad.com>
2019-01-23 19:24:41 +0530

    TA-81698 BOM Status prep service related changes.

--- src/main/java/com/ambr/gtm/fta/qps/bom/qualstatus/TradeLaneDetail.java
+++ src/main/java/com/ambr/gtm/fta/qps/bom/qualstatus/TradeLaneDetail.java
@@ -93,7 +93,6 @@ public class TradeLaneDetail
 		if (this.systemDecision == STPDecisionEnum.I) {
 			
 		}
-		
 		if (theQualTX.qualified_flg == null) {
 			this.qualEvalStatus = QualificationEvaluationStatusEnum.UNKNOWN;
 		}
