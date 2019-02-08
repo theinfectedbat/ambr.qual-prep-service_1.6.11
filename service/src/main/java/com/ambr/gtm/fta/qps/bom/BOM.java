20b1ea61dfa3543649646acfb033723802543a7b
m2269 <m2269@lt-101941.amberroad.com>
2019-02-08 16:07:01 -0500

    Removed obsolete method

--- service/src/main/java/com/ambr/gtm/fta/qps/bom/BOM.java
+++ service/src/main/java/com/ambr/gtm/fta/qps/bom/BOM.java
@@ -359,19 +359,6 @@ public class BOM
 	}
 	
 	
-	private void indexByAltKeyBOMComponent()
-	{
-		this.altKeyBOMComponentIndex = new HashMap<Long, BOMComponent>();
-		
-		if (this.compList != null)
-		{
-			for (BOMComponent bomComponent : this.compList)
-			{
-				this.altKeyBOMComponentIndex.put(bomComponent.alt_key_comp, bomComponent);
-			}
-		}
-	}
-
 	/**
      *************************************************************************************
      * <P>
