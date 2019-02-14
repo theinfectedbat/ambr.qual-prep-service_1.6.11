56b93865636bf906f11992c697f799a8d9b1ab20
m2427 <m2427@MDLPTP241.amberroad.com>
2019-02-14 11:40:42 +0530

    TA-83814
    In Save as BOM updated component HS number is not pulled

--- src/main/java/com/ambr/gtm/fta/qts/workmgmt/QTXWorkUniverse.java
+++ src/main/java/com/ambr/gtm/fta/qts/workmgmt/QTXWorkUniverse.java
@@ -817,9 +817,9 @@ public class QTXWorkUniverse
 		
 		if (container == null)
 		{
-			logger.info("@@@GPMIVAContainer.execute API() invoked at : " + new Timestamp(System.currentTimeMillis()));
+			logger.info("GPMIVAContainer.execute API() invoked at : " + new Timestamp(System.currentTimeMillis()));
 			container = this.gpmSourceIVAByProductFromUniverseClientAPI.execute(prodKey);
-			logger.info("@@@GPMIVAContainer.execute API() completed at :" + new Timestamp(System.currentTimeMillis()));
+			logger.info("GPMIVAContainer.execute API() completed at :" + new Timestamp(System.currentTimeMillis()));
 			if (container != null && container.prodKey == prodKey)
 			{
 				this.gpmSourceIVAProductCache.put(prodKey, container);
@@ -887,9 +887,9 @@ public class QTXWorkUniverse
 		
 		if (product == null)
 		{
-			logger.info("@@@GPM CLassiofication.execute API() invoked at: " + new Timestamp(System.currentTimeMillis()));
+			logger.info("GPM CLassiofication.execute API() invoked at: " + new Timestamp(System.currentTimeMillis()));
 			product = this.gpmClassificationsByProductAPI.execute(prodKey);
-			logger.info("@@@GPM CLassiofication.execute API() completed at :" + new Timestamp(System.currentTimeMillis()));
+			logger.info("GPM CLassiofication.execute API() completed at :" + new Timestamp(System.currentTimeMillis()));
 			if (product != null && product.prodKey == prodKey)
 			{
 				this.gpmClassificationCache.put(prodKey, product);
@@ -918,9 +918,9 @@ public class QTXWorkUniverse
 			{
 				try
 				{
-					logger.info("@@@BOMUniverseBOMClientAPI.execute API() invoked at: " + new Timestamp(System.currentTimeMillis()));
+					logger.info("BOMUniverseBOMClientAPI.execute API() invoked at: " + new Timestamp(System.currentTimeMillis()));
 					BOM bom = this.bomUniverseBOMClientAPI.execute(bomKey);
-					logger.info("@@@BOMUniverseBOMClientAPI.execute API() completed at: " + new Timestamp(System.currentTimeMillis()));  
+					logger.info("BOMUniverseBOMClientAPI.execute API() completed at: " + new Timestamp(System.currentTimeMillis()));  
 					if(bom == null) throw new Exception("Bom not found for the key : "+bomKey);
 					
 					for (WorkPackage workPackage : this.workByBOMMap.get(bomKey).values())
