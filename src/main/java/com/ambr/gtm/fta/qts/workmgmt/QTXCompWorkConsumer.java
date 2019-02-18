eb6aba31ae487be255f72f0ba874fe1d2715b5be
a8332 <a8332@MDDESK375.amberroad.com>
2019-02-18 19:40:58 +0530

    TA-83750
    Java Null Pinter Exception in QTXCompWork Consumer

--- src/main/java/com/ambr/gtm/fta/qts/workmgmt/QTXCompWorkConsumer.java
+++ src/main/java/com/ambr/gtm/fta/qts/workmgmt/QTXCompWorkConsumer.java
@@ -414,11 +414,13 @@ public class QTXCompWorkConsumer extends QTXConsumer<CompWorkPackage>
 				if (compWorkIVA.isReasonCodeFlagSet(RequalificationWorkCodes.GPM_COMP_TRACE_VALUE_CHANGE))
 				{
 					GPMClaimDetailsSourceIVAContainer claimdetailsContainer = aClaimsDetailCache.getClaimDetails(compWorkIVA.iva_key);
+					if(null != claimdetailsContainer){
 					GPMClaimDetails aClaimDetails = claimdetailsContainer.getPrimaryClaimDetails();
 					String ftaCodeGroup = QualTXUtility.determineFTAGroupCode(qualtx.org_code, qualtx.fta_code, qtxBusinessLogicProcessor.propertySheetManager);
 					Map<String,String> flexFieldMap = getFeildMapping("STP", ftaCodeGroup);
 					setTracedValue(qualtxComp, aClaimDetails, flexFieldMap); //TA-82724
 					qualtxComp.traced_value_currency =(String) aClaimDetails.getValue(flexFieldMap.get("TRACED_VALUE_CURRENCY"));
+					}
 				}
 				if (compWorkIVA.isReasonCodeFlagSet(RequalificationWorkCodes.GPM_COMP_CUMULATION_CHANGE))
 				{
